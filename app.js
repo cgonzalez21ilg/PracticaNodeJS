65e0db8acb3cf9d6f005819e0ee06a7a68f72f8b
cgonzalez21ilg <cgonzalez21@ilg.cat>
2022-05-14 00:39:23 +0200

    Primer Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/users', isAuthorized, (req,res) => {
+    res.json([{
+        id: 1,
+        name: 'Carlos'
+    },
+    {
+        id: 2,
+        name: 'Lucia'
+
+    }])    
+})
+
+app.get('/product', (req,res) => {
+    const products = [
+    {
+      id: 1,
+      name: "hammer",
+    },
+    {
+      id: 2,
+      name: "screwdriver",
+    },
+    ,
+    {
+      id: 3,
+      name: "wrench",
+    },
+   ];
+  
+   res.json(products);
+  })
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+function isAuthorized(req,res, next) {
+  const auth = req.headers.authorization;
+  if (auth === '1234') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
+
